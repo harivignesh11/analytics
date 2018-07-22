7aced9fa1f2c7735d14da39443fe7a925c5fd037
harivignesh11 <harivignesh11@gmail.com>
2018-07-22 09:35:07 +0530

    2nd commit

---
+++ data structures.R
@@ -0,0 +1,40 @@
+(mymatrix=matrix(1:12,byrow=2,ncol=6))
+mymatrix[2:3,3:4]
+colSums(mymatrix)
+rowMeans(mymatrix)
+mymatrix[1,1] = 4
+(mymatrix)
+marks <- rnorm(60,mean=60,sd=15)
+marks
+matrix(marks,ncol=6)
+(myarray= array(1:23, dim=c(4,3,2)))
+apply(myarray,1,sum)
+?array
+#nominal data are data like gender#
+#ordinal data are rankings and ratings#
+
+(rollno= c('MBA',1:60,sep='-'))
+(rollno= paste('MBA',1:60,sep='-'))
+(name= paste('Student',1:60,sep='-'))
+marks<-round(marks)
+df=data.frame(rollno,name)
+(gender=sample(c('M','F'),size = 60,replace=T, prob=c(.4,.5)))
+table(gender)
+prop.table(table(gender))
+grade=sample(c('A','B','C'),size = 60,replace=T, prob=c(.2,.3,.5))
+(grade=factor(grade,levels = c('C','B','A'),ordered = T))
+(df=data.frame(rollno,name,gender,marks, stringsAsFactors = F,grade))
+str(df)
+head(df, n=3)  
+(df$gender=factor(df$gender))
+summary(df)
+dim(df)
+class(df)
+(aggregate(df$marks,by=list(df$gender),FUN=mean))
+(aggregate(marks~gender +grade,data=df,FUN=mean))
+(aggregate(df$marks,by=list(df$gender),FUN=mean))
+df$marks2=runif(60,50,100)
+head(df)
+cbind(grade,gender)
+rbind(grade,gender)
+df[df$gender=='M',c(1,2,4)]
--- vectors.R
+++ vectors.R
@@ -1,8 +1,8 @@
 # Data structures - Vectors used for one single row or single column#
 #to run the code select the code that is required to be run#
 x=c(1,5,7,8,4) 
-x2 <- c(1,2,3,4)
-x[2]
+x2 <- c(1,2,3,4,5)
+x[5]
 x2
 x4=c('m','f','m','f','m','f','m','f','m','f','m','f','m','f')
 
@@ -16,4 +16,11 @@ x5= 1:100 #1:100 pushes numbers from 1-100 to x5#
 marks <- rnorm(60,mean=60,sd=10)
 marks
 plot(density(marks))
+print(median(x2))
+?class
+class(v1)
+class(v2)
+v2=c('a')
+v1=c('a','b','c')
+v1[v1 %in% v2]
 
